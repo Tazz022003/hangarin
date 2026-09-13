300cdcf0f73b9e21565524c935e995ed6ce78ebc
Tazz022003 <almoceracharsgerand@gmail.com>
2026-09-13 16:16:15 +0800

    configure allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9pdwa)ttft(cs##*p((0@%^k*rnwn@(s500d&mr(rpa&w38$@2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["hangarin.pythonanywhere.com", "127.0.0.1", "localhost"]
 
 
 # Application definition
