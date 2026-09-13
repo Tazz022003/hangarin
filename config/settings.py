93f93fcec970c719b29d3bcac6405ce142f0cf17
Tazz022003 <almoceracharsgerand@gmail.com>
2026-09-13 16:21:37 +0800

    modifird static files

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9pdwa)ttft(cs##*p((0@%^k*rnwn@(s500d&mr(rpa&w38$@2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["hangarin.pythonanywhere.com", "127.0.0.1", "localhost"]
 
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Email
